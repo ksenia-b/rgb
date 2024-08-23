7b3b5396b86f6ccab36c2b8079fc99bbaf8ab4d0
ksenia-b <ksenia.barshchyk@gmail.com>
2024-08-23 17:49:13 +0300

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.listen(3001, () => {
+    console.log('Hello World');
+});
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API");
+});
