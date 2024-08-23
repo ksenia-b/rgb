faa018a8a6397ba08d4a3213154fc4827b67e40a
ksenia-b <ksenia.barshchyk@gmail.com>
2024-08-24 00:32:42 +0300

    added schema, routes, model for File

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
-// const User = require('../models/user.model');
+const User = require('../models/user.model');
 
 router.get("/:userId", async (req, res) => {
     try {
-        console.log("find user by id here", req.params.userId)
+        const user = await User.findById(req.params.userId).populate({
+            path: 'files',
+            options: {
+                skip: (page - 1) * limit,
+                limit: parseInt(limit)
+            }
+        });
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
     }
     catch (error) {
         res.status(500).send(error.message);
