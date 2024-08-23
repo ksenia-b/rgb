da54ecf8182e12c4e8a3e9e1896951a147cde2f6
ksenia-b <ksenia.barshchyk@gmail.com>
2024-08-23 23:15:54 +0300

    added mongo connection

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const userRoutes = require('./routes/user.routes');
 const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user.routes');
+
+require('dotenv').config();
+
 const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error('Connection error:', error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 3002;
-
 app.listen(PORT, () => {
-    console.log("Server is running on port 3002");
+    console.log(`Server is running on port ${PORT}`);
 });
